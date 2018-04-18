e29bb98a95dea941bc6695f77bcf6f175ad83e0f
Claire Gammon <claire.gammon@gmail.com>
2018-04-18 13:10:32 -0600

    things

--- knexfile.js
+++ knexfile.js
@@ -5,9 +5,8 @@ module.exports = {
   development: {
     client: 'mysql',
     connection: {
-      host     : '127.0.0.1',
-      user     : 'root',
-      password : 'gammon',
+      unixSocket : '/var/run/mysqld/mysqld.sock',
+      user     : 'clairescout',
       database : 'redthread',
       charset  : 'utf8'
     }
